afe65e10ca0869513deec81d98473d38fc7a2693
Bonnie Schulkin <bonnie@learnersguild.com>
2018-01-29 10:25:49 -0800

    Live code review 1/26

--- src/server/allfiles.js
+++ src/server/allfiles.js
@@ -4,60 +4,62 @@ const queries = require('../models/files');
 
 //route to allfiles - get - read all files
 router.get('/', (req, res, next) => {
-  return queries
-    .listAllFiles()
+  return queries.listAllFiles()
     .then(allfiles => {
-      res.status(200).json({ allfiles });
+      res.status(200).json({allfiles});
     })
     .catch(next);
 });
 
+
 //route to allfiles - post - create new file
 router.post('/', (req, res, next) => {
-  return queries
-    .saveFile(req.body)
-    .then(result => {
-      res.status(200).json(result);
+  return queries.saveFile(req.body)
+    .then((result) => {
+      res.status(200).json(result)
     })
     .catch(next);
 });
+
 
 //route to one file - get one file
 router.get('/:fileID', (req, res, next) => {
   const fileID = parseInt(req.params.fileID);
-  return queries
-    .getFileContent(fileID)
+
+  return queries.getFileContent(fileID)
     .then(content => {
-      res.status(200).json(content);
+      res.status(200).json(content)
     })
-    .catch(next);
-});
+    .catch(next)
+})
+
 
 //route to update file content
 router.put('/:fileID', (req, res, next) => {
   const fileID = parseInt(req.params.fileID);
-  const { newContent } = req.body;
-  return queries
-    .updateFileContent(fileID, newContent)
+  const {newContent} = req.body;
+
+  return queries.updateFileContent(fileID, newContent)
     .then(() => {
       res.status(200).json({});
     })
-    .catch(next);
+    .catch(console.error);
 });
+
 
 //route to delete one file
 router.delete('/:fileID', (req, res, next) => {
   const fileID = parseInt(req.params.fileID);
-  return queries
-    .deleteFile(fileID)
+
+  return queries.deleteFile(fileID)
     .then(() => {
       res.status(200).json({});
     })
-    .catch(next);
+    .catch(next)
 });
 
-router.use((err, req, res) => {
-  res.json({ error: true, message: err.toString() });
-});
+router.use((err, req, res, next) => {
+  res.json({error: true, message: err.toString()})
+})
 
-module.exports = router;
+modules.export = router
