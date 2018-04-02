4471fcb83e4d3164fbffcecaccf43f0990b37211
Bonnie Schulkin <bonnie.schulkin@gmail.com>
2018-04-02 15:51:12 -0700

    Initial pass on comments; still more files to review

--- src/server/allfiles.js
+++ src/server/allfiles.js
@@ -2,62 +2,63 @@ const express = require('express');
 const router = express.Router();
 const queries = require('../models/files');
 
-//route to allfiles - get - read all files
+// bds: put space between // and comment
+// route to allfiles - get - read all files
 router.get('/', (req, res, next) => {
-  return queries
-    .listAllFiles()
+  // bds: I would put queries.listAllFiles() on one line, as that's the intial call
+  // bds: otherwise, it seems like queries is a function call
+  return queries.listAllFiles()
     .then(allfiles => {
       res.status(200).json({ allfiles });
     })
     .catch(next);
 });
 
-//route to allfiles - post - create new file
+// route to allfiles - post - create new file
 router.post('/', (req, res, next) => {
-  return queries
-    .saveFile(req.body)
+  return queries.saveFile(req.body)
     .then(result => {
       res.status(200).json(result);
     })
     .catch(next);
 });
 
-//route to one file - get one file
+// route to one file - get one file
 router.get('/:fileID', (req, res, next) => {
   const fileID = parseInt(req.params.fileID);
-  return queries
-    .getFileContent(fileID)
+  return queries.getFileContent(fileID)
     .then(content => {
       res.status(200).json(content);
     })
     .catch(next);
 });
 
-//route to update file content
+// route to update file content
 router.put('/:fileID', (req, res, next) => {
   const fileID = parseInt(req.params.fileID);
   const { newContent } = req.body;
-  return queries
-    .updateFileContent(fileID, newContent)
+  return queries.updateFileContent(fileID, newContent)
     .then(() => {
       res.status(200).json({});
     })
     .catch(next);
 });
 
-//route to delete one file
+// route to delete one file
 router.delete('/:fileID', (req, res, next) => {
   const fileID = parseInt(req.params.fileID);
-  return queries
-    .deleteFile(fileID)
+  return queries.deleteFile(fileID)
     .then(() => {
       res.status(200).json({});
     })
     .catch(next);
 });
 
+// bds: some comments would be good here, similar to how you commented the routes above
 router.use((err, req, res) => {
   res.json({ error: true, message: err.toString() });
 });
 
 module.exports = router;
+
+// bds: overall, this file looks great! :-D
